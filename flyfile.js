302d410b7dd0554cbcb461ce6c2464e696d76d45
12px <dalton@12px.io>
2017-06-28 14:56:53 -0500

    initial commit.

---
+++ flyfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fin = './dist/';
+const src = './src/**/*.css';
+const css = './src/BLANQ.css';
+const min = { suffix: '.min' };
+const cfg = {
+  to: fin,
+  from: css,
+  map: false,
+  plugins: [
+    require('postcss-import'),
+    require('postcss-cssnext'),
+    require('cssnano')
+  ]
+};
+
+exports.default = function * (fly) {
+  yield fly.start('build')
+  yield fly.watch(`${src}`, 'build')
+};
+
+exports.build = function * (fly) {
+  yield fly.source(`${css}`).postcss(cfg).rename(min).target(`${fin}`);
+};
