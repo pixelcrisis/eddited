16b4f31feaf6933947e2b9940af250b36c269621
12px <dalton@12px.io>
2017-07-06 11:39:54 -0500

    add support for options.

--- taskfile.js
+++ taskfile.js
@@ -1,42 +1,39 @@
 'use strict';
 
-const $ = {
-  v: require('./package.json').version,
-  cssnano: require('cssnano'),
-  import: require('postcss-import'),
-  csscomb: require('postcss-csscomb')('zen'),
-  cssnext: require('postcss-cssnext')({ features: { fontVariant: false } })
-};
+const version = require('./package.json').version;
+const csscond = require('postcss-conditionals');
+const cssvars = require('postcss-simple-vars');
+const csscomb = require('postcss-csscomb');
+const cssnext = require('postcss-cssnext');
+const imports = require('postcss-import');
+const cssnano = require('cssnano');
 
 const cfg = {
-  clean: { plugins: [ $.csscomb ] },
-  build: {
-    to: './dist/', from: './src/theme.css',
-    plugins: [ $.import, $.cssnext, $.cssnano ]
-  },
-  rename: { suffix: '.min' }
+  rename: { suffix: '.min' },
+  clean: { plugins: [ csscomb ] },
+  build: { from: './src/theme.css', to: './dist',
+    plugins: [ imports, cssvars, csscond, cssnext, cssnano ] }
+};
+
+/* version prefix plugin */
+const prefix = function * (file) {
+  let data = file[0].data.toString();
+  file[0].data = new Buffer(`/* v${version} */\n${data}`);
 };
 
 exports.default = function * (task) {
-  yield task.serial(['clean', 'build'])
+  yield task.serial( ['clean', 'build'] )
   yield task.watch('src/**/*.css', 'build')
+};
+
+exports.clean = function * (task) {
+  yield task.source('src/**/*.css').postcss(cfg.clean).target('src');
 };
 
 exports.build = function * (task) {
   yield task.source('src/theme.css')
     .postcss(cfg.build)
     .rename(cfg.rename)
-    .run({ every: false },
-      function * (file) {
-        let data = file[0].data.toString();
-        let contents = `/* v${$.v} */ \n${data}`;
-        file[0].data = new Buffer(contents);
-      })
-    .target('dist');
-};
-
-exports.clean = function * (task) {
-  yield task.source('src/**/!(_root).css')
-    .postcss(cfg.clean)
-    .target('src');
+    .run({ every: false }, prefix)
+    .target('dist')
 };
