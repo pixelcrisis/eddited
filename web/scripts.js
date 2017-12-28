483ae840062c3992afcfcf1759391d1dce52a884
12px <dalton@12px.io>
2017-12-27 19:36:47 -0600

    web compiler works

--- web/scripts.js
+++ web/scripts.js
@@ -3,7 +3,9 @@ var start = new Vue({
   el: '#eddit',
   data: {
     toRestore: '',
-    config: config
+    config: config,
+    imports: null,
+    compiled: '',
   },
   methods: {
     is: function(type, label) {
@@ -42,6 +44,43 @@ var start = new Vue({
 
       this.config = processing;
       alert('Imported!');
+    },
+    loadFiles: function(callback) {
+      if (!this.imports) {
+        var _get = new XMLHttpRequest();
+        _get.onreadystatechange = function () {
+          if (this.readyState == 4) {
+            this.imports = this.response;
+            callback(this.response);
+          }
+        }
+        _get.open("GET", '../src/build/web.less', true);
+        _get.send();
+      }
+      else {
+        return callback(this.imports);
+      }
+    },
+    compile: function() {
+      let that = this;
+      let lessVars = {};
+      that.compiled = '';
+      for (var i = 0;i < config.length;i++) {
+        var name = config[i].name;
+        lessVars[name] = config[i].data.toString();
+        if (this.is('@text', name)) lessVars[name] = `"${lessVars[name]}"`;
+      }
+      console.log(lessVars);
+      this.loadFiles(function(imports){
+        less.render(imports, { modifyVars: lessVars, relativeUrls: true }, function(e,o) {
+          if (e) return console.log(e);
+
+          new CleanCSS().minify(o.css, function (err, out) {
+            if (!err) that.compiled = out.styles;
+            else console.log(err);
+          });
+        })
+      });
     }
   }
 });
