7819d3a02d98972c673200c8b7cd740477c23f3b
12px <dalton@12px.io>
2017-12-29 15:43:07 -0600

    readme n stuff

---
+++ README.md
@@ -0,0 +1,136 @@
+# r/eddited
+A back-to-basics subreddit theme. Flat, minimal, with a focus on content, eddited is arguably reddit's cleanest and most customizable subreddit theme.
+
+## Contents
+- Colors
+- Options
+- Plugins
+
+
+### Colors
+
+- `@color-accent`  
+Blue by default; The main accent color of the site.  
+
+- `@color-background`  
+White by default; Background color.  
+
+- `@color-text`  
+Black by default; Main font color.  
+
+- `@color-text-subtle`  
+A lighter/darker version of the main font color, to blend in easier with the background.
+
+- `@color-shadow`  
+Should only be a few shades lighter/darker than *@color-background*
+
+- `@color-border`  
+Follows the same guide as *@color-shadow* but even lighter/darker.
+
+- `@color-link`  
+The color for the links on the page. Usually matches *@color-accent*
+
+- `@color-button`  
+The color for the buttons on the page. Usually matches *@color-accent*
+
+- `@color-button-text`  
+A readable color for button text, usually *@color-background*
+
+- `@color-thumbnail`  
+The background for thumbnails. Usually matches *@color-shadow*
+
+- `@color-upvote`  
+Usually an orange color.
+
+- `@color-downvote`  
+Usually a blue color.
+
+- `@color-nsfw`  
+The NSFW badge/thumbnail color. Usually red.
+
+- `@color-locked`  
+Locked badge/thumbnail color. Usually gold.
+
+- `@color-stickied`  
+Sticked thread color, usually green.
+
+- `@color-visited`  
+Color for visited links, should be a subtle color like *@color-text-subtle*
+
+- `@color-red-flair`  
+The color for red flairs, usually matches *@color-nsfw*
+
+- `@color-green-flair`  
+The color for green flairs, usually matches *@color-stickied*
+
+### Options
+
+- `@width-sidebar`  
+The width of the sidebar.
+
+- `@width-thumbnail / @height-thumbnail`  
+Change the size/aspect ratio of the thumbnail
+
+- `@text-submit-link`  
+Submit Link button text; "Show" by default
+
+- `@text-submit-text`  
+Submit Text button text; "Tell" by default
+
+- `@text-subscribers`  
+Subscribers text; "Users" by default
+
+- `@text-here-now`  
+Here Now text; "Browsing" by default
+
+- `@text-sub-prefix`
+The prefix for subreddit name.
+
+
+### Plugins
+
+#### Header box
+- `@enable-header-box`  
+This enables/disabled the header image area above the main reddit page.  
+
+- `@header-box-height`  
+
+- `@header-box-bg-color`  
+
+- `@header-box-text-color`  
+
+- `@header-box-bg-image`  
+Use a background image in the header box. Filename 'header'
+
+- `@text-header-box`  
+The splash text. "r/eddited - clean. simple."
+
+
+#### Sidebar Image
+- `@enable-sidebar-image`  
+Enable an image in the sidebar. Filename 'sidebar'
+
+- `@sidebar-image-height`
+
+
+#### Sidebar Popout
+- `@enable-sidebar-popout`  
+Only show certain information on sidebar hover.
+
+#### Headline
+Text is the first H1 element in the sidebar.  
+`# This will show up as a headline`
+
+- `@enable-headline-box`  
+Enables the box below the header.
+
+- `@headline-box-bg-color`  
+
+- `@headline-box-text-color`  
+
+#### Split Submit Buttons
+- `@enable-split-submit-buttons`
+
+
+#### NSFW thumbnails
+- `@enable-nsfw-thumbnails`
--- web/scripts.js
+++ web/scripts.js
@@ -1,80 +1,73 @@
+/*=== eddited web compiler ===*/
+
+// contains
+String.prototype.is = function(string) { return this.indexOf(string) > -1; };
+var str = function(str) { return String.prototype.toString(str); };
+
 // Start Vue
 var start = new Vue({
   el: '#eddit',
+
   data: {
-    toRestore: '',
-    config: config,
-    imports: null,
+    toload: '',
+    imported: '',
     compiled: '',
+    config: config
   },
+
   methods: {
-    is: function(type, label) {
-      if (typeof label === 'boolean') return false;
-      return label.toLowerCase().indexOf(type) > - 1;
+    is: function(type, data) {
+      let name = data.name;
+      let value = data.data + "";
+      if (type == "size") return (value.is('px'));
+      else if (type == "text") return (name.is('@text'));
+      else if (type == "color") return (name.is('@color') || value.is('#'));
+      else if (type == "toggle") {
+        return (name.is('@enabled') || value.is('true') || value.is('false'));
+      }
     },
-    isColor: function(conf) {
-      if (this.is('@color', conf.name)) return true;
-      if (this.is('#', conf.data)) return true;
-      return false;
-    },
-    isText: function(conf) {
-      return this.is('@text', conf.name);
-    },
-    isPx: function(conf) {
-      return this.is('px', conf.data);
-    },
-    isToggle: function(conf) {
-      if (this.is('@enable', conf.name)) return true;
-      if (typeof conf.data === 'boolean') return true;
-      return false;
-    },
-    labelMaker: function(label) {
-      return label.split('-').join(' ').replace('@', '');
-    },
+
+    label: function(str) { return str.split('-').join(' ').replace('@', ''); },
+
     restore: function() {
-      if (!this.toRestore) return alert('Nothing to compile.');
-      if (this.toRestore.charAt(0) != "[") return alert('Syntax Err 1');
-      if (this.toRestore.charAt(this.toRestore.length - 1) != "]") return alert('Syntax Err 2');
+      if (!this.imported) return alert('Nothing to compile!');
       try {
-        var processing = JSON.parse(this.toRestore);
+        var processing = JSON.parse(this.imported);
       } catch (e) {
-        if (e.name == 'SyntaxError') return alert('Syntax Error 3');
+        if (e.name == 'SyntaxError') return alert('Syntax Error!');
         else return alert(e.message);
       }
-
       this.config = processing;
       alert('Imported!');
     },
-    loadFiles: function(callback) {
-      if (!this.imports) {
-        var _get = new XMLHttpRequest();
-        _get.onreadystatechange = function () {
-          if (this.readyState == 4) {
-            this.imports = this.response;
-            callback(this.response);
-          }
+
+    loadFiles: function(cb) {
+      if (this.toload) return cb(this.toload);
+      var _get = new XMLHttpRequest();
+      _get.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          this.toload = this.response;
+          cb(this.response);
         }
-        _get.open("GET", '../src/build/web.less', true);
-        _get.send();
-      }
-      else {
-        return callback(this.imports);
-      }
+      };
+      _get.open("GET", "../src/build/web.less", true);
+      _get.send();
     },
+
     compile: function() {
       let that = this;
       let lessVars = {};
       that.compiled = '';
+
       for (var i = 0;i < config.length;i++) {
         var name = config[i].name;
-        lessVars[name] = config[i].data.toString();
-        if (this.is('@text', name)) lessVars[name] = `"${lessVars[name]}"`;
+        lessVars[name] = config[i].data + "";
+        if (this.is('text', config[i])) lessVars[name] = `"${lessVars[name]}"`;
       }
-      console.log(lessVars);
+
       this.loadFiles(function(imports){
         less.render(imports, { modifyVars: lessVars, relativeUrls: true }, function(e,o) {
           if (e) return console.log(e);
-
           new CleanCSS().minify(o.css, function (err, out) {
             if (!err) that.compiled = out.styles;
             else console.log(err);
